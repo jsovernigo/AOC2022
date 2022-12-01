1efaa0b0477862fec4fadd90f76e51bbed34398f
Julian <gsoverni@uoguelph.ca>
2022-12-01 10:56:31 -0500

    Project setup; problem 01/1 complete.

---
+++ 01/d1p1/src/main.rs
@@ -0,0 +1,29 @@
+use std::io::{BufRead, BufReader};
+use std::fs::File;
+use std::vec::Vec;
+
+fn main() {
+    let file = File::open("d1p1.txt").expect("Should have been able to open file.");
+    let reader = BufReader::new(file);
+
+    let mut elfindex: usize = 0;
+    let mut maxindex: usize = 0;
+    let mut elves: Vec<i32> = Vec::new();
+    elves.push(0);
+
+    for line in reader.lines() {
+        let rline = line.expect("Line should resolve, not fail.");
+        if rline == "" {
+            if elves[elfindex] > elves[maxindex] {
+                maxindex = elfindex;
+            }
+
+            elfindex += 1;
+            elves.push(0);
+        } else {
+            elves[elfindex] += rline.parse::<i32>().unwrap();
+        }
+    }
+    println!("Elf with most calories: Elf {} with {} calories.", maxindex, elves[maxindex]);
+
+}
