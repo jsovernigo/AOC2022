f9c977795153e059fa0a6bb01f021c68e46aafd1
Julian <gsoverni@uoguelph.ca>
2022-12-06 09:53:59 -0500

    Added day6 p1, p2. p1/2 behaviour controlled by maker_length.

---
+++ day6/src/main.rs
@@ -0,0 +1,91 @@
+use std::fs::File;
+use std::io::{BufReader, BufRead};
+
+fn main() {
+    let file = File::open("input.txt").unwrap();
+    let reader = BufReader::new(file);
+    
+    // 4 for start-of-coms markers, 14 for start-of-message
+    let marker_length = 14; 
+
+    // for each line in the coms file (see test.txt)
+    for line in reader.lines() {
+
+        // check if line unwraps from error
+        match line {
+
+            // in the valid case,
+            Ok(l) => {
+                    let mut i = 0;
+                    let mut iter = l.bytes();
+                    let mut history: Vec<u8> = Vec::new();
+
+                    // collect the minimum history
+                    for _i in 0..(marker_length - 1) {
+
+                        match iter.next() {
+
+                            // a new history character
+                            Some(n) => {
+                                    history.insert(0, n);
+                                    i += 1
+                                },
+
+                            // too little coms content for a marker to exist
+                            None => panic!("too few items to populate history."),
+                        }
+                    }
+
+                    
+                    // loop for the rest of the line
+                    loop {
+
+                        match iter.next() {
+
+                            // a new character occurred
+                            Some(n) =>
+
+                                // if this character exists in the history, or
+                                if history.contains(&n) || 
+
+                                        // history contains...
+                                        history
+                                        .iter()
+
+                                        // any element i
+                                        .any(|i| 
+                                            
+                                            // that if filtered by
+                                            history.iter()
+                                                .filter(|&o| *o == *i)
+
+                                                // results in more than one result
+                                                .count() > 1) {
+
+                                    // get rid of oldest history character and insert the newest.
+                                    history.pop();
+                                    history.insert(0, n);
+                                    i += 1;
+
+                                // i.e. no characters are repeated in history: marker found.
+                                } else {
+                                    println!("First marker found at {}", i + 1);
+                                    break;
+                                },
+
+                            // this line does not contain a marker.
+                            None =>  {
+                                println!("this line does not contain a marker.");
+                                break;
+                            }
+                        }
+                    }
+
+                },
+
+            // line had some error occur during assignment - this is likely EOF.
+            Err(_) =>
+                break,
+        }
+    }
+}
