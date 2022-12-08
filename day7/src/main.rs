0a9662195cc2c167964071bc55371d9381668bef
Julian <gsoverni@uoguelph.ca>
2022-12-08 10:02:52 -0500

    Added day 7 part 1. a bit behind.

--- day7/src/main.rs
+++ day7/src/main.rs
@@ -4,33 +4,150 @@ use scanner::inner_scan;
 
 use std::fs::File;
 use std::io::{BufReader, BufRead};
-//use trees::Tree;
+use std::pin::Pin;
+
+
+use trees::{Tree, Node};
+
+struct Directory {
+    name: String,
+    files: Vec<(String, usize)>,
+    cachedsize: Option<usize>,
+}
+
+impl std::fmt::Display for Directory {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> Result<(), std::fmt::Error> {
+        write!(f, "Dir {} with {} files\n", self.name, self.files.len())
+
+    }
+}
+
+impl Directory {
+    fn new(name: String) -> Directory {
+        return Directory { 
+            name: name,
+            files: Vec::new(), 
+            cachedsize: None,
+        }
+    }
+    
+    fn update_cached_size(&mut self, newsize: usize) {
+        match self.cachedsize {
+            Some(n) => self.cachedsize = Some(n + newsize),
+            None => self.cachedsize = Some(newsize)
+        }
+    }
+
+    fn add(&mut self, filename: String, size: usize) {
+        self.files.push((filename, size));
+        self.update_cached_size(size);
+    }
+
+    fn get_cached_size(&self) -> usize {
+        return match self.cachedsize {
+            Some(size) => size,
+            None => 0,
+        };
+    }
+
+}
+
+
+fn construct_tree_from_lines(
+    line_iter: &mut impl Iterator <Item = Result<String, std::io::Error>>, 
+    mut current: Pin<&mut Node<Directory>>) {
+
+    loop {
+        match line_iter.next() {
+            Some(line) =>
+                match line {
+                    Ok(rline) => {
+
+                        // line with a command.
+                        if rline.contains("$") {
+
+                            if rline.contains("cd") {
+                                let (dirname, ) = scanf!(&rline, "$ cd {}", String);
+                              
+                                // abort from this depth.
+                                if dirname == ".." {
+                                    return
+                                }
+
+                                // otherwise, just descend to the next level.
+                                for child in current.iter_mut() {
+                                    if child.data().name == dirname {
+                                        construct_tree_from_lines(line_iter, child);
+                                    }
+                                }
+                                
+                            } else if rline.contains("ls") {
+                                // ignore, currently.
+                            }
+
+                        // line with a "dir" entry.
+                        } else if rline.contains("dir") {
+                            let (dirname, ) = scanf!(&rline, "dir {}", String);
+                            current.push_back(
+                                Tree::new(
+                                Directory::new(dirname)
+                                )
+                            );
+
+                        // line with a file entry.
+                        } else {
+                            let (size, name) = scanf!(&rline, "{} {}", usize, String);
+                            current.data_mut().add(name, size);
+                        }
+                    },
+                    Err(_) =>
+                        panic!("Why did we encounter an io error during reading?")
+                }
+
+            None => break,
+        }
+    }
+
+}
+
+fn get_sum_of_directory(current: &Node<Directory>) -> usize {
+    if current.has_no_child() {
+        return current.data().get_cached_size();
+    } else {
+        let mut directorysize: usize = current.data().get_cached_size();
+
+        for child in current.iter() {
+            directorysize += get_sum_of_directory(child);
+        }
+        directorysize
+    }
+}
+
+fn get_sum_less_than_100k(current: &Node<Directory>) -> usize {
+    let mut less_than_100ksum: usize = 0;
+    let directory_sum: usize = get_sum_of_directory(current);
+
+    for child in current.iter() {
+        less_than_100ksum += get_sum_less_than_100k(child);
+    }
+
+    if directory_sum < 100_000 {
+        less_than_100ksum += directory_sum;
+    }
+
+    less_than_100ksum
+}
 
 fn main() {
 
-    let file = File::open("test.txt").unwrap();
+    let file = File::open("input.txt").unwrap();
     let reader = BufReader::new(file);
 
-    //let ftree = Tree::new();
+    let mut ftree: Tree<Directory> = Tree::new(Directory::new("/".to_string()));
 
-    let mut currentdirectory = String::from("/");
+    construct_tree_from_lines(&mut reader.lines().skip(1), ftree.root_mut());
 
-    for line in reader.lines() {
-        let rline = line.unwrap();
-
-        println!("Got line {}", rline);
-
-        // cmd line.
-        if rline.starts_with("$") {
-            let (fname,) = scanf!(&rline, "$ cd {}", String);
-            currentdirectory = fname;
-        // file output line.
-        } else {
-            let (size, name) = scanf!(&rline, "{} {}", usize, String);
-            println!("Found file {} of size {} in dir {}.", name, size, currentdirectory);
-        }
-
-    }
-    
+    let ftree = ftree;
+    println!("{}", get_sum_less_than_100k(ftree.root()));
 
 }
